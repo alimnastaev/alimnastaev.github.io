fa24c766b27aa92b1974a2e0a61c842288b70126
alimnastaev <a.nastaev@gmail.com>
2017-11-08 19:13:46 -0500

    first upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# CLfinalproject
+
+Test
---
+++ js/carousel.js
@@ -0,0 +1,11 @@
+/* image carousel */
+$('#carousel img:gt(0)').hide();
+
+setInterval(function(){
+  $('#carousel :first-child')
+    .fadeTo(500, 0)
+    .next('img')
+    .fadeTo(500, 1)
+    .end()
+    .appendTo('#carousel');
+}, 3000);   
